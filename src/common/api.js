f1127a29c2afc62d0d84c88c2cb86ea69fbc0d01
Andrew Asplund <andrew.j.asplund@gmail.com>
2019-05-25 21:53:47 -0700

    It might be done

--- src/common/api.js
+++ src/common/api.js
@@ -9,8 +9,7 @@ export const API = axios.create({
 API.interceptors.request.use(function (config) {
     // Set common parameters for each request to the API.
 
-    // TODO: Use your own APPID for OpenWeatherMap.org
-    config.params.APPID = 'YOUR_APPID_HERE';
+    config.params.APPID = '0152b91eb4c200e41680a8f3b37af976';
     config.params.units = 'imperial';
 
     return config;
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import VueLocalStorage from 'vue-ls';
 
-// TODO: Add vue-ls support by importing an configuring vue-ls
 
+let options = {
+  namespace: 'weather_'
+};
+
+Vue.use(VueLocalStorage, options);
 
 Vue.config.productionTip = false
 
